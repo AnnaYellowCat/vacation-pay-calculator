365e1797d34ae0e2d4893ff7f642c248b130d2f2
Anna <anna.gudkova.rus@mail.ru>
2025-04-02 20:03:57 +0400

    date list transfer fixed

--- src/main/java/com/neoflex/vacation_pay_calculator/dto/VacationPayRequest.java
+++ src/main/java/com/neoflex/vacation_pay_calculator/dto/VacationPayRequest.java
@@ -1,5 +1,6 @@
 package com.neoflex.vacation_pay_calculator.dto;
 
+import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Positive;
 
 import java.time.LocalDate;
@@ -12,20 +13,40 @@ public class VacationPayRequest {
     private Integer vacationDaysNumber;
     private List<LocalDate> vacationDates;
 
-    public VacationPayRequest(double averageSalary, int vacationDaysNumber){
+    public VacationPayRequest() {
+    }
+
+    public VacationPayRequest(double averageSalary, int vacationDaysNumber) {
         this.averageSalary = averageSalary;
         this.vacationDaysNumber = vacationDaysNumber;
     }
 
-    public double getAverageSalary(){
+    public double getAverageSalary() {
         return averageSalary;
     }
 
-    public int getVacationDaysNumber(){
+    public void setAverageSalary(double averageSalary) {
+        this.averageSalary = averageSalary;
+    }
+
+    public Integer getVacationDaysNumber() {
         return vacationDaysNumber;
     }
 
-    public List<LocalDate> getVacationDates(){
+    public void setVacationDaysNumber(Integer vacationDaysNumber) {
+        this.vacationDaysNumber = vacationDaysNumber;
+    }
+
+    public List<LocalDate> getVacationDates() {
         return vacationDates;
+    }
+
+    public void setVacationDates(List<LocalDate> vacationDates) {
+        this.vacationDates = vacationDates;
+    }
+
+    @AssertTrue(message = "Either vacationDaysNumber or vacationDates must be provided")
+    public boolean isVacationInputValid() {
+        return vacationDaysNumber != null || (vacationDates != null && !vacationDates.isEmpty());
     }
 }
